b57f5c81705d597f5e30e7e74ec0f5dfc2b9e6d4
1508814348@qq.com <1508814348@qq.com>
2026-01-30 17:26:16 +0800

    tj

--- bus_route_correction/public/console/javascript/console.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  ui.custom.console.init(configs.drawBoard);
-  $("#p-nav").append($("#nav"));
-  var lang = window.i18n.init({
-    cn: "./javascript/i18n/console.cn.json",
-    en: "./javascript/i18n/console.en.json",
-  });
-  $("#lang").val(lang);
-});
